53ce2806b5f1a6a04af8cf0232da3884470d575a
Shreeyut Maheshwari <149173158+shreeyut1905@users.noreply.github.com>
2026-06-18 21:28:34 +0530

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,1 @@
+python main.py --prompt-path test.json --output save_results.jsonl --clip-model-path microsoft/BiomedCLIP-PubMedBERT_256-vit_base_patch16_224 --max-new-tokens 256 --visual-top-k 30 --window-size 8 --batch-size 16 --model-path MedR1_VQA
